dcf7a3999e63c517118978032486f1e82834feb5
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-06-04 13:33:24 +0300

    added useDeferredValue

--- src/example/Concurrent.tsx
+++ src/example/Concurrent.tsx
@@ -1,31 +1,32 @@
-import React, {ChangeEvent, useMemo, useState, useTransition} from 'react';
+import React, {ChangeEvent, useDeferredValue, useMemo, useState} from 'react';
 import {defaultItems} from './defaultItems';
 
 const Concurrent = () => {
 	const [value, setValue] = useState('');
-	const [filteredValue, setFilteredValue] = useState('');
+	// const [filteredValue, setFilteredValue] = useState('');
 	const [items, setItems] = useState(defaultItems);
+	const deferedValue = useDeferredValue(value);
 
-	const [isPending, startTransition] = useTransition();
+	// const [isPending, startTransition] = useTransition();
 
 	const filterItems = useMemo(() => {
 		setItems(defaultItems);
 		return items.filter((item) =>
-			item.title.toLowerCase().includes(filteredValue),
+			item.title.toLowerCase().includes(deferedValue),
 		);
-	}, [filteredValue]);
+	}, [deferedValue]);
 
 	const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
 		setValue(e.target.value);
-		startTransition(() => {
-			setFilteredValue(e.target.value);
-		});
+		// startTransition(() => {
+		// 	setFilteredValue(e.target.value);
+		// });
 	};
 
 	return (
 		<div style={{margin: '20px auto'}}>
 			<input type='text' value={value} onChange={onChangeValue} />
-			{isPending && <h1>Loading...</h1>}
+			{/*{isPending && <h1>Loading...</h1>}*/}
 			{filterItems.map((item) => (
 				<div key={item.id}>
 					<div>id={item.id}</div>
